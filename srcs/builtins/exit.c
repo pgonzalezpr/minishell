da10efa6e0e18e2743820ad4c57ffed3cd280c91
brayans22 <bsaiago@fi.uba.ar>
2024-03-29 18:49:48 +0100

    exit STDERR

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: brayan <brayan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 18:42:17 by brayan            #+#    #+#             */
-/*   Updated: 2024/03/29 18:31:16 by brayan           ###   ########.fr       */
+/*   Updated: 2024/03/29 18:46:25 by brayan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,18 @@ static int	is_numeric_exit_status(char *args)
 */
 static void	exec_exit(t_minishell *minishell, char **args, int exit, int err)
 {
-	printf("%s\n", EXIT_CMD);
 	if (exit == 255 && err)
 	{
+		ft_putstr_fd(EXIT_CMD, STDERR_FILENO);
+		ft_putchar_fd(LINE_BREAK, STDERR_FILENO);
 		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
 		ft_putstr_fd(args[1], STDERR_FILENO);
 		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+	}
+	else
+	{
+		ft_putstr_fd(EXIT_CMD, 1);
+		ft_putchar_fd(LINE_BREAK, 1);
 	}
 	rl_clear_history();
 	clear_history();
@@ -53,7 +59,11 @@ int	builtin_exit(t_minishell *minishell, char **args)
 	if (!args[1])
 		exec_exit(minishell, args, EXIT_SUCCESS, 0);
 	else if (args[2])
+	{
+		ft_putstr_fd(EXIT_CMD, STDERR_FILENO);
+		ft_putchar_fd(LINE_BREAK, STDERR_FILENO);
 		ft_putstr_fd(MSG_TOO_MANY_ARGS_EXIT, STDERR_FILENO);
+	}
 	else if (!is_numeric_exit_status(args[1]))
 		exec_exit(minishell, args, 255, 1);
 	else
